71b7888843ee1fd92761d7b91823d1841b590bdf
Aurora Shido-Wagenet and Ward Chamberlain <aurora@>
2019-03-06 16:21:39 -0800

    it will have scroll effects

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -1,9 +1,19 @@
 $(function() {
+
+  var $test = $(".lever");
+  var $lever = $(".lever-test");
+
   var controller = new ScrollMagic.Controller();
   var tlRightSwipe = new TimelineMax()
-    .fromTo("#gray-container", 1, {x: "100%"}, {x: "0%", ease: Linear.easeNone, delay: "2"})
-    .fromTo(".square-dude", 1, {x: '0'}, {x:'1000'})
-    .fromTo("#yellow-container", 1, {x: "100%"}, {x:"0%", ease: Linear.easeNone, delay: "4"});
+    .fromTo("#gray-container", 1, {x: "100%"}, {x: "0%", ease: Linear.easeNone})
+    .to(".television", 3, {"backgroundPositionY": "-750px"})
+    .to(".stone-tv", 2, {left: '-30%'})
+    .fromTo("#yellow-container", 2, {x: "100%"}, {x:"0%", ease: Linear.easeNone, delay: "2"})
+    .fromTo(".square-dude", 12, {left: '5%'}, {left:'80%', ease: Linear.easeNone}, '-=8')
+    .to(".grass-background", 12, {backgroundPositionX : "-30%"}, 0)
+    .to(".grass-foreground", 12, {backgroundPositionX : "-80%"}, 0)
+    .to(".trees-background", 12, {backgroundPositionX : "-2%"}, 0)
+    .to(".trees-foreground", 12, {backgroundPositionX : "-10%"}, 0)
 
   var scene = new ScrollMagic.Scene({
     triggerElement: "#container",
@@ -15,5 +25,14 @@ $(function() {
   .addIndicators()
   .addTo(controller);
 
+  //lever puzzle TweenMax
 
+
+
+  $test.on("click", function(){
+    $(".lever").toggleClass('active');
+    var tween = TweenMax.to(".television", 1, {
+      backgroundPositionX: '-750px'
+    });
+  });
 });
